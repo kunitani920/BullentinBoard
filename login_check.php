08c63a34b89478801c2ce51c77948cd008d748ee
tani <55948775+kunitani920@users.noreply.github.com>
2020-02-01 16:52:09 +0900

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -8,58 +8,74 @@ require_once 'validation/passwordValidation.php';
 session_start();
 
 class Login
-{
-  private $clean = array();
+    {
+    private $clean = array();
 
-  public function __construct()
-  {
-    $this->clean = sanitize::clean($_POST);
+    public function __construct()
+    {
+        $this->clean = sanitize::clean($_POST);
 
-    //validation
-    $email_validation = new emailValidation();
-    $is_email = $email_validation->isEmail($this->clean['email']);
-    $password_validation = new passwordValidation();
-    $is_password = $password_validation->isPassword($this->clean['password']);
+        //validation
+        $email_validation = new emailValidation();
+        $is_email = $email_validation->isEmail($this->clean['email']);
+        $password_validation = new passwordValidation();
+        $is_password = $password_validation->isPassword($this->clean['password']);
 
-    //validationエラー
-    if (!$is_email || !$is_password) {
-        $_SESSION['email_error'] = $email_validation->getErrorMessage();
-        $_SESSION['password_error'] = $password_validation->getErrorMessage();
-        $_SESSION['email'] = $this->clean['email'];
-        $_SESSION['first_visit'] = 'off';
-        header('Location: login.php');
-        exit();
+        //validationエラー
+        if (!$is_email || !$is_password) {
+            $_SESSION['email_error'] = $email_validation->getErrorMessage();
+            $_SESSION['password_error'] = $password_validation->getErrorMessage();
+            $_SESSION['email'] = $this->clean['email'];
+            $_SESSION['first_visit'] = 'off';
+            header('Location: login.php');
+            exit();
+        }
     }
-  }
 
-  public function main()
-  {
-    //DB接続
-    $db = new Db();
-    $pdo = $db->dbconnect();
-    //membersテーブル参照
-    $collation = $pdo->prepare('SELECT * FROM members WHERE email=?');
-    $collation->execute(array($this->clean['email']));
-    $member = $collation->fetch();
-    //jinjiテーブル参照
-    $collation = $pdo->prepare('SELECT * FROM jinji WHERE email=?');
-    $collation->execute(array($this->clean['email']));
-    $jinji = $collation->fetch();
+    public function main()
+    {
+        //DB接続
+        $db = new Db();
+        $pdo = $db->dbconnect();
+        //membersテーブル参照
+        $collation = $pdo->prepare('SELECT * FROM members WHERE email=?');
+        $collation->execute(array($this->clean['email']));
+        $member = $collation->fetch();
+        //jinjiテーブル参照
+        $collation = $pdo->prepare('SELECT * FROM jinji WHERE email=?');
+        $collation->execute(array($this->clean['email']));
+        $jinji = $collation->fetch();
 
-    //新規登録（登録emailなし）
-    if (!$member && !$jinji) {
-        $_SESSION['email'] = $this->clean['email'];
-        $_SESSION['password'] = $this->clean['password'];
-        $_SESSION['first_visit'] = 'on';
-        $pdo = null;
-        header('Location: ./new/new_intention.php');
-        exit();
-    } 
-    
-    //memberログイン成功（email,password一致）
-    if ($member && $member['password'] === $this->clean['password']) {
-        // $_SESSION['id'] = $member['id'];
-        // $_SESSION['time'] = time();
+        //新規登録（登録emailなし）
+        if (!$member && !$jinji) {
+            $_SESSION['email'] = $this->clean['email'];
+            $_SESSION['password'] = $this->clean['password'];
+            $_SESSION['first_visit'] = 'on';
+            $pdo = null;
+            header('Location: ./new/new_intention.php');
+            exit();
+        } 
+        
+        //memberログイン成功（email,password一致）
+        if ($member && $member['password'] === $this->clean['password']) {
+            // $_SESSION['id'] = $member['id'];
+            // $_SESSION['time'] = time();
+            
+            //ログイン情報記録
+            // if($this->clean['save'] === 'on') {
+            //   setcookie('email', $this->clean['email'], time()+60*60*24*14);
+            //   setcookie('password', $this->clean['password'], time()+60*60*24*14);
+            // }
+            $pdo = null;
+            header('Location: list.php');
+            exit();
+        }
+
+        //jinjiログイン成功（email,password一致）
+        if ($jinji && $jinji['password'] === $this->clean['password']) {
+        // $_SESSION['id'] = $jinji['id'];
+        //   $_SESSION['time'] = time();
+        //jinji用のフラグ
         
         //ログイン情報記録
         // if($this->clean['save'] === 'on') {
@@ -69,32 +85,16 @@ class Login
         $pdo = null;
         header('Location: list.php');
         exit();
+        }
+
+        //ログイン失敗（email一致、password不一致）
+        $_SESSION['mismatch_error'] = 'メールアドレスとパスワードが一致しません' . PHP_EOL;
+        $_SESSION['email'] = $this->clean['email'];
+        $_SESSION['first_visit'] = 'off';
+        $pdo = null;
+        header('Location: login.php');
+        exit();
     }
-
-    //jinjiログイン成功（email,password一致）
-    if ($jinji && $jinji['password'] === $this->clean['password']) {
-      // $_SESSION['id'] = $jinji['id'];
-    //   $_SESSION['time'] = time();
-      //jinji用のフラグ
-      
-      //ログイン情報記録
-      // if($this->clean['save'] === 'on') {
-      //   setcookie('email', $this->clean['email'], time()+60*60*24*14);
-      //   setcookie('password', $this->clean['password'], time()+60*60*24*14);
-      // }
-      $pdo = null;
-      header('Location: list.php');
-      exit();
-    }
-
-    //ログイン失敗（email一致、password不一致）
-    $_SESSION['mismatch_error'] = 'メールアドレスとパスワードが一致しません' . PHP_EOL;
-    $_SESSION['first_visit'] = 'off';
-    header('Location: login.php');
-
-    $pdo = null;
-    exit();
-  }
 }
 
 $login = new Login();
